5c00423474dce6a610e618663f6e5dfa6f9105cd
Ricardo_Garcia <rygarcia@ucdavis.edu>
2023-02-14 07:47:10 -0800

    IN PROGRESS

--- studio1/script.js
+++ studio1/script.js
@@ -2,45 +2,31 @@
     'use strict';
     console.log('reading js');
 
-    const myForm = document.querySelector('#myform');
+     const myForm = document.querySelector('#myform');
     const madlib = document.querySelector('#madlib');
 
-    myForm.addEventListener('submit', function(event){
+    document.querySelector('#submit').addEventListener('click', function(event){
         event.preventDefault();
+        console.log('hi');
 
-
-
-        
+    
         const noun1 = document.querySelector('#noun1').value;
         const noun2 = document.querySelector('#noun2').value;
         const adj = document.querySelector('#adj').value;
         const verb = document.querySelector('#verb').value;
 
 
+   document.querySelector('#page2').style.display="block"; 
+   document.querySelector('#page1').style.display="none";    
+   
+        document.querySelector('#adjfill').innerText=adj;
+        document.querySelector('#nounfill').innerText=noun1;
+        document.querySelector('#verbfill').innerText=verb;
+        document.querySelector('#noun2fill').innerText=noun2;
+        document.querySelector('#adjectivefill').innerText=adjective;
+
         let mytext;
 
-        if(noun1==''){
-           mytext = `Please provide a noun`;
-           document.querySelector('#noun1').focus();
-        } else if (noun2==''){
-            mytext = `Please provide a second noun`;
-            document.querySelector('#noun2').focus();   
-        } else if (adj==''){
-            mytext = `Please provide an adjective`;
-            document.querySelector('#adj').focus();  
-        } else if (verb==''){
-            mytext = `Please provide a verb`;
-            document.querySelector('#verb').focus();  
-
-        } else {
-            mytext = `You typed the words ${noun1}, ${noun2}, ${adj}, and ${verb}`
-            
-            document.querySelector('#noun1').value="";
-            document.querySelector('#noun2').value="";
-            document.querySelector('#adj').value="";
-            document.querySelector('#verb').value="";
-
-        }
-        madlib.innerHTML = mytext;
+        
     })
 }())
