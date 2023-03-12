98c94885d83921753bbe3f38454daba081d2c236
Ricardo_Garcia <rygarcia@ucdavis.edu>
2023-03-12 16:22:37 -0700

    new

---
+++ final /version2/script.js
@@ -0,0 +1,26 @@
+(function(){
+    'use strict';
+    console.log('reading js');
+
+    const images = document.querySelectorAll('.image');
+    const overlay = document.querySelector('.overlay');
+    const close = document.querySelectorAll('.close');
+    
+    for(let i=0; i< images.length; i++){
+        images[i].addEventListener('click', function(event){
+            event.preventDefault();
+            console.log(event.target.nextElementSibling);
+            const thisOverlay = event.target.nextElementSibling;
+            thisOverlay.className='overlay showing';
+            close[i].addEventListener('click', function(){
+               thisOverlay.className='overlay hidden';
+            })
+        })
+
+    }
+   
+
+
+    
+    
+})();
